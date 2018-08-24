d4d224c3c14368be6b825fcc2e6a9632b3f1a72f
Eder Marques <eder.marques@citi.org.br>
2018-08-24 14:21:52 -0300

    add phone error message

--- main.js
+++ main.js
@@ -2,23 +2,27 @@ $("document").ready(() => {
   $("form").submit(e => {
     e.preventDefault();
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($('input[name=email]').val())) {
-      $.post(
-        "https://simples-email.herokuapp.com/",
-        {
-          data:
-            "Nome: " +
-            $("input[name=name]").val() +
-            "\nEmail: " +
-            $("input[name=email]").val() +
-            "\nTelefone: " +
-            $("input[name=phone]").val() +
-            "\nInstagram: " +
-            $("input[name=instagram]").val()
-        },
-        data => {
-          console.log(data);
-        }
-      );
+      if($("input[name=phone]").val().length >= 16){
+        $.post(
+          "https://simples-email.herokuapp.com/",
+          {
+            data:
+              "Nome: " +
+              $("input[name=name]").val() +
+              "\nEmail: " +
+              $("input[name=email]").val() +
+              "\nTelefone: " +
+              $("input[name=phone]").val() +
+              "\nInstagram: " +
+              $("input[name=instagram]").val()
+          },
+          data => {
+            console.log(data);
+          }
+        );
+      } else {
+        $('#error-phone').show();
+      }
     } else {
       $('#error-email').show();
     }
@@ -26,5 +30,5 @@ $("document").ready(() => {
   $('.form-input').click(() => {
     $('#error-email').hide();
   });
-  $("input[name=phone]").mask("(00) 0 0000-000");
+  $("input[name=phone]").mask("(00) 0 0000-0000");
 });
