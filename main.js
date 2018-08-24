a545fb9930acc87ded5900e71c2b6d6b8e8fb844
Eder Marques <eder.marques@citi.org.br>
2018-08-24 14:14:52 -0300

    add validation to email and phone

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 $("document").ready(() => {
   $("form").submit(e => {
     e.preventDefault();
-    $.post('https://simples-email.herokuapp.com/', {
-      data: "Nome: "+$('input[name=name]').val()+"\nEmail: "+$('input[name=email]').val()+"\nTelefone: "+$('input[name=phone]').val()+"\nInstagram: "+$('input[name=instagram]').val()
-    }, (data) => {
-      console.log(data);
-    });
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($('input[name=email]').val())) {
+      $.post(
+        "https://simples-email.herokuapp.com/",
+        {
+          data:
+            "Nome: " +
+            $("input[name=name]").val() +
+            "\nEmail: " +
+            $("input[name=email]").val() +
+            "\nTelefone: " +
+            $("input[name=phone]").val() +
+            "\nInstagram: " +
+            $("input[name=instagram]").val()
+        },
+        data => {
+          console.log(data);
+        }
+      );
+    } else {
+      $('#error-email').show();
+    }
   });
+  $('.form-input').click(() => {
+    $('#error-email').hide();
+  });
+  $("input[name=phone]").mask("(00) 0 0000-000");
 });
