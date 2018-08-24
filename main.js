636c7943ef1178c2cba7ac1cfaf755ffcd3f12a7
Eder Marques <eder.marques@citi.org.br>
2018-08-24 13:03:47 -0300

    add assets

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $("document").ready(() => {
       "Nome: "+$('input[name=name]').val()+"\nEmail: "+$('input[name=email]').val()+"\nTelefone: "+$('input[name=phone]').val()+"\nInstagram: "+$('input[name=instagram]').val(),
       { token: "9cbd6d8c-f707-4e56-8578-0f1767063445" }
     );
-    console.log("Nome: "+$('input[name=name]').val()+"\nEmail: "+$('input[name=email]').val()+"\nTelefone: "+$('input[name=phone]').val()+"\nInstagram: "+$('input[name=instagram]').val());
+    // console.log("Nome: "+$('input[name=name]').val()+"\nEmail: "+$('input[name=email]').val()+"\nTelefone: "+$('input[name=phone]').val()+"\nInstagram: "+$('input[name=instagram]').val());
   });
 });
 
